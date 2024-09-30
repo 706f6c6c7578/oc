afc0487d5ab3af1906f7acdfc35a0fb16653f70e
Stefan <stefan.claas@mail.ru>
2024-09-30 20:03:14 +0200

    Add files via upload

--- oc_paganini_server.go
+++ oc_paganini_server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -16,7 +18,7 @@ import (
 const (
 	password = "secretPassword" // Set your desired password here
 	server   = "bofhteamhroxbmd6pxbjrg6egqrnnu2vj7vlxpcnb3ypk56devuyj6yd.onion:119" // Replace this with your NNTP server and port
-	torProxy = "127.0.0.1:9050"         // Default Tor SOCKS proxy address
+	torProxy = "127.0.0.1:9050" // Default Tor SOCKS proxy address
 )
 
 func main() {
@@ -56,11 +58,11 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 		writer.Close()
 	}()
 
-	// Call the sendArticle function (previously in pn.go)
-	err = sendArticle(reader)
+	// Call the sendArticle function and get the session log
+	sessionLog, err := sendArticle(reader)
 	if err != nil {
 		log.Println("Error sending article:", err)
-		http.Error(w, "Error sending article", http.StatusInternalServerError)
+		http.Error(w, "Error sending article: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -73,20 +75,22 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(os.Stderr, "File %s received at %s by %s\n", header.Filename, currentTime, username)
 
 	// Output to the client
-	fmt.Fprintf(w, "File %s received and sent!", header.Filename)
+	fmt.Fprintf(w, "File %s received and sent!\n\nNNTP Session Log:\n%s", header.Filename, sessionLog)
 }
 
-func sendArticle(reader io.Reader) error {
+func sendArticle(reader io.Reader) (string, error) {
+	var sessionLog bytes.Buffer
+
 	// Create a SOCKS5 dialer using the Tor proxy
 	dialer, err := proxy.SOCKS5("tcp", torProxy, nil, proxy.Direct)
 	if err != nil {
-		return fmt.Errorf("error creating SOCKS5 dialer: %v", err)
+		return "", fmt.Errorf("error creating SOCKS5 dialer: %v", err)
 	}
 
 	// Establish a connection to the NNTP server through Tor
 	conn, err := dialer.Dial("tcp", server)
 	if err != nil {
-		return fmt.Errorf("error connecting to the server through Tor: %v", err)
+		return "", fmt.Errorf("error connecting to the server through Tor: %v", err)
 	}
 	defer conn.Close()
 
@@ -94,23 +98,25 @@ func sendArticle(reader io.Reader) error {
 	bufReader := bufio.NewReader(conn)
 	response, err := bufReader.ReadString('\n')
 	if err != nil {
-		return fmt.Errorf("error when reading the server greeting: %v", err)
+		return "", fmt.Errorf("error when reading the server greeting: %v", err)
 	}
-	fmt.Print("Server: ", response)
+	sessionLog.WriteString("Server: " + response)
 
 	// Send POST command
 	fmt.Fprintf(conn, "POST\r\n")
+	sessionLog.WriteString("OnionC: POST\r\n")
 	response, err = bufReader.ReadString('\n')
 	if err != nil {
-		return fmt.Errorf("error reading the POST response: %v", err)
+		return "", fmt.Errorf("error reading the POST response: %v", err)
 	}
-	fmt.Print("Server: ", response)
+	sessionLog.WriteString("Server: " + response)
 
 	if !strings.HasPrefix(response, "340") {
-		return fmt.Errorf("server does not accept POST")
+		return "", fmt.Errorf("server does not accept POST")
 	}
 
 	// Read and send articles from the reader
+	sessionLog.WriteString("OnionC: [Article content not logged]\n")
 	scanner := bufio.NewScanner(reader)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -118,23 +124,30 @@ func sendArticle(reader io.Reader) error {
 	}
 
 	if err := scanner.Err(); err != nil {
-		return fmt.Errorf("error reading the input: %v", err)
+		return "", fmt.Errorf("error reading the input: %v", err)
 	}
 
 	// Send end of article
 	fmt.Fprintf(conn, ".\r\n")
+	sessionLog.WriteString("OnionC: .\r\n")
 
 	// Read response from the server
 	response, err = bufReader.ReadString('\n')
 	if err != nil {
-		return fmt.Errorf("error reading the server response: %v", err)
+		return "", fmt.Errorf("error reading the server response: %v", err)
 	}
-	fmt.Print("Server: ", response)
+	sessionLog.WriteString("Server: " + response)
 
 	// Send QUIT command
 	fmt.Fprintf(conn, "QUIT\r\n")
-	io.Copy(os.Stdout, bufReader)
+	sessionLog.WriteString("OnionC: QUIT\r\n")
 
-	return nil
+	// Read the final server response
+	finalResponse, err := ioutil.ReadAll(bufReader)
+	if err != nil {
+		return "", fmt.Errorf("error reading the final server response: %v", err)
+	}
+	sessionLog.Write(finalResponse)
+
+	return sessionLog.String(), nil
 }
-
