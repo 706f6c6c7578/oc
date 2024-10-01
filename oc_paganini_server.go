2886da18d4f1dc0dd90b2765eafb789dad866118
Stefan <stefan.claas@mail.ru>
2024-10-01 22:56:07 +0200

    Update oc_paganini_server.go

--- oc_paganini_server.go
+++ oc_paganini_server.go
@@ -116,7 +116,7 @@ func sendArticle(reader io.Reader) (string, error) {
 	}
 
 	// Read and send articles from the reader
-	sessionLog.WriteString("OCServ: Article not stored by Onion Courier.\nNo log files are written.]\n")
+	sessionLog.WriteString("OCServ: Data not stored nor logged by Usenet Onion Gateway.\n")
 	scanner := bufio.NewScanner(reader)
 	for scanner.Scan() {
 		line := scanner.Text()
