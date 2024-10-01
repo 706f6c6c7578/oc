ac290562ed527abcb24b7fa4f7fc855b559770be
Stefan <stefan.claas@mail.ru>
2024-10-01 23:14:44 +0200

    Update oc_m2n_server.go

--- oc_m2n_server.go
+++ oc_m2n_server.go
@@ -145,7 +145,7 @@ func sendMail(message []byte) (string, error) {
 	sessionLog.WriteString("QUIT command sent\n")
 
 	fmt.Println("Message sent and not stored.")
-	sessionLog.WriteString("Data not stored nor log files are written, by onion mail2news proxy.")
+	sessionLog.WriteString("Data not stored nor log files are written, by mail2news onion proxy.")
 
 	return sessionLog.String(), nil
 }
