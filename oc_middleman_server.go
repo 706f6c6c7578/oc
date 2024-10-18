8eed8fbdb3da27ce37ed58b74c4a942f8f8a913f
Stefan <stefan.claas@mail.ru>
2024-10-18 22:48:07 +0200

    Update oc_middleman_server.go

--- oc_middleman_server.go
+++ oc_middleman_server.go
@@ -99,7 +99,7 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 
     responseMsg := "File received and sent.\n"
     responseMsg += "No data is stored or logged by Onion Courier.\n\n"
-    responseMsg += "Target Onion Courier Response: %s\n"
+    responseMsg += "Onion Courier Response: %s\n"
 
     fmt.Fprintf(w, responseMsg, response)
 }
